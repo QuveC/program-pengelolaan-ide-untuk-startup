c1effae2a2d6d1fa1f2e3edc79a6791601a421bb
ahmaddddddddddd <140603183+ahmaddddddddddd@users.noreply.github.com>
2025-05-15 07:48:12 +0700

    struct

--- main.go
+++ main.go
@@ -1,6 +1,45 @@
 package main
 import "fmt"
 
+
+const maxIdea = 100
+const maxTitle = 100
+
+type Idea struct {
+	ID     int
+	Title  [maxTitle]string
+	Upvote int
+}
+
+var ideas [maxIdea]Idea
+
+
+
+func createIdea(){
+
+
+}
+
+func updateIdea(){
+
+
+}
+
+func deleteIdea(){
+  
+
+}
+
+func upVote(){
+
+
+}
+
+func popularIdea(){
+ fmt.Print
+
+}
+
 func main(){
   fmt.Print("test")
 
