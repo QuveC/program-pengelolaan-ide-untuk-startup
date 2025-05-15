d42a5b008b35dac5387608605d59c2d0da65f234
ahmaddddddddddd <140603183+ahmaddddddddddd@users.noreply.github.com>
2025-05-15 07:54:37 +0700

    Input Menu

--- main.go
+++ main.go
@@ -36,11 +36,32 @@ func upVote(){
 }
 
 func popularIdea(){
- fmt.Print
-
+     
 }
 
-func main(){
-  fmt.Print("test")
+func main() {
+	var pilihan int
 
+	for {
+		fmt.Println("\n=== MENU UTAMA ===")
+		fmt.Println("1. Tambah Ide")
+		fmt.Println("2. Voting")
+		fmt.Println("3. List Popular Idea")
+		fmt.Println("4. Keluar")
+		fmt.Print("Pilih menu: ")
+		fmt.Scanln(&pilihan)
+
+		if pilihan == 1 {
+			createIdea()
+		} else if pilihan == 2 {
+      popularIdea()
+		} else if pilihan == 3 {
+			popularIdea()
+		}else if pilihan == 4{
+      		fmt.Println("Terima kasih.")
+			break
+    }else {
+      fmt.Println("Pilihan tidak ada")
+    }
+  }
 }
